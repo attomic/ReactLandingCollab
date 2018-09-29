96237299d1eb4a000e6f18d204442aebf200eafe
Sergio Herrera <regonline@bellsouth.net>
2018-09-29 22:57:33 +0000

    First Commit

---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+// import { Button} from 'react-bootstrap';
+
+export const NavBar = () => {
+    
+    return(
+        <nav className="navbar navbar-expand navbar-dark bg-dark fixed-top">
+            <div className="container">
+                <a className="navbar-brand" href="#">Start Bootstrap</a>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home<span className="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Services</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {NavBar} from './Navbar.jsx';
+import {Jumbotron} from './Jumbotron.jsx';
 
-//include images into your bundle
-import rigoImage from '../../img/rigo-baby.jpg';
+//here you tell react to put <NavBar /> inside the #myApp DOM element 
 
-//create your first component
 export class Home extends React.Component{
     
     render(){
         return (
-            <div className="text-center mt-5">
-                <h1>Hello Rigo!</h1>
-                <p><img src={rigoImage} /></p>
-                <a href="#" className="btn btn-success">If you see this green button... bootstrap is working</a>
-                <p>Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with love!</p>
+            <div>
+                <NavBar/>
+                <Jumbotron/>
             </div>
         );
     }
